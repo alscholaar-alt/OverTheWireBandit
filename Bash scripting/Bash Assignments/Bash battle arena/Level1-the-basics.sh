865d566dd2ceefc96223f7b563b51be47e50ea28
Abdulhamid Mohamed <alscholaar@gmail.com>
2026-02-12 23:49:58 -0500

    Update and rename challenge1.md to Level1-the-basics.sh

---
+++ Bash scripting/Bash Assignments/Bash battle arena/Level1-the-basics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Mission: Create a directory named Arena and then inside it, create three files: warrior.txt, mage.txt, and archer.txt. List the contents of the Arena directory.
+
+mkdir Arena
+cd Arena
+touch {warrior,mage,archer}.txt
+ls Arena
